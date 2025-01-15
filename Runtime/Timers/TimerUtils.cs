4828160417a980baa5c2a55a370f3d348dd3b042
yonnii :) <starlightdevii@yahoo.com>
2025-01-15 20:46:03 +0000

    Style/VisualElement Extensions

---
+++ Runtime/Timers/TimerUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Yonii.Unity.Utilities.Timers
+{
+    public static class TimerUtils
+    {
+        public static float CountDown(this float time)
+        {
+            time -= Time.deltaTime;
+            return time;
+        }
+        
+        public static bool HasFinished(this float time) => time <= 0;
+    }
+}
