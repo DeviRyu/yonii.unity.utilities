6b111c8105d5f918b1f541007a11e5ebf3850087
yonnii :) <starlightdevii@yahoo.com>
2025-06-22 21:48:12 +0100

    Add Transform and Vector3 Forward extension methods
    Introduced TransformExtensions with a Forward method for calculating a point in front of a Transform. Added a similar Forward method to Vector3Extensions. Updated the assembly definition to set the root namespace and changed the package display name.

---
+++ Runtime/TransformExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Yonii.Unity.Utilities
+{
+    public static class TransformExtensions
+    {
+        public static Vector3 Forward(this Transform transform, float distance)
+        {
+            return transform.position + transform.forward * distance;
+        }
+    }
+}
--- Runtime/Vector3Extensions.cs
+++ Runtime/Vector3Extensions.cs
@@ -6,5 +6,8 @@ namespace Yonii.Unity.Utilities
     {
         public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null) =>
             new(x ?? vector.x, y ?? vector.y, z ?? vector.z);
+        
+        public static Vector3 Forward(this Vector3 position, Vector3 forwardDirection, float distance) => 
+            position + forwardDirection.normalized * distance;
     }
 }
